2ae4b38751631e9e4f502130f78abce7417362ef
triedtofu <raymond.du4@gmail.com>
2024-04-20 23:01:42 +1000

    signup api route created

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -7,14 +7,14 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    password: {
-      type: String,
-      required: true,
-    },
     email: {
       type: String,
       required: true,
       unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
     },
   },
   { timestamps: true }

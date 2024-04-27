aa332a59c1acea1e74bbafc5c28a1e26d206b763
triedtofu <raymond.du4@gmail.com>
2024-04-27 23:24:13 +1000

    completed sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
-import SignOut from './pages/SignUp';
+import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/About';
 import Header from './components/Header';
@@ -14,7 +14,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/signout" element={<SignOut />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/about" element={<About />} />
       </Routes>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="flex items-start justify-center min-h-screen pt-10">
+      <div className="bg-white shadow-lg rounded p-12">
+        <h1 className="text-2xl text-center font-semibold mb-4">
+          {' '}
+          Create your Propel account
+        </h1>
+        <form className="flex flex-col gap-5">
+          Email
+          <br />
+          <input type="text" className="border p-3 rounded-md" id="email" />
+          Name
+          <br />
+          <input type="text" className="border p-3 rounded-md" id="username" />
+          Password
+          <br />
+          <input type="text" className="border p-3 rounded-md" id="password" />
+          <button className="bg-indigo-800 text-white p-3 mt-4 rounded-md uppercase hover:opacity-90 disabled:opacity-50">
+            Sign Up
+          </button>
+        </form>
+        <div className="flex gap-1 mt-8 justify-center">
+          <p>Have an account?</p>
+          <Link to={'/signin'}>
+            <span className="text-indigo-500">Sign in</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }

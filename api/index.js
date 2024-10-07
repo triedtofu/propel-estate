2270bfa43c87cafcc54223af1477c7e4a42be268
triedtofu <raymond.du4@gmail.com>
2024-10-07 22:23:43 +1100

    ready for render

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ mongoose
     console.log('err');
   });
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -25,6 +28,12 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
